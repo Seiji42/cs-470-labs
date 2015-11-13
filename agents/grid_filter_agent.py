601609202f5a8ff79615b678ad632f1035c1b81a
Tyler Monson <princenosnow@gmail.com>
2015-11-13 10:55:17 -0700

    Start grid filter hmwk

---
+++ agents/grid_filter_agent.py
@@ -0,0 +1,166 @@
+#!/usr/bin/python -tt
+
+# An incredibly simple agent.  All we do is find the closest enemy tank, drive
+# towards it, and shoot.  Note that if friendly fire is allowed, you will very
+# often kill your own tanks with this code.
+
+#################################################################
+# NOTE TO STUDENTS
+# This is a starting point for you.  You will need to greatly
+# modify this code if you want to do anything useful.  But this
+# should help you to know how to interact with BZRC in order to
+# get the information you need.
+#
+# After starting the bzrflag server, this is one way to start
+# this code:
+# python agent0.py [hostname] [port]
+#
+# Often this translates to something like the following (with the
+# port name being printed out by the bzrflag server):
+# python agent0.py localhost 49857
+#################################################################
+
+import sys
+import math
+import time
+import OpenGL
+OpenGL.ERROR_CHECKING = False
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+import numpy as np
+
+from bzrc import BZRC, Command
+
+class Agent(object):
+    """Class handles all command and control logic for a teams tanks."""
+
+    def __init__(self, bzrc):
+        self.bzrc = bzrc
+        self.constants = self.bzrc.get_constants()
+        self.commands = []
+        self.starting_prob = 0.5
+        self.grid = np.ones((int(self.constants['worldsize']), int(self.constants['worldsize'])))
+        self.grid.fill(self.starting_prob)
+        # self.init_window(800,800)
+        print self.constants
+
+    def tick(self, time_diff):
+        """Some time has passed; decide what to do next."""
+
+        print self.bzrc.get_occgrid(0)
+        #self.update_grid(self.grid)
+        #self.draw_grid()
+        mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
+        self.mytanks = mytanks
+        self.othertanks = othertanks
+        self.flags = flags
+        self.shots = shots
+        self.enemies = [tank for tank in othertanks if tank.color !=
+                        self.constants['team']]
+
+        self.commands = []
+
+        for tank in mytanks:
+            self.attack_enemies(tank)
+
+        results = self.bzrc.do_commands(self.commands)
+
+    def explore_grid(self, tank):
+        return
+
+    def attack_enemies(self, tank):
+        """Find the closest enemy and chase it, shooting as you go."""
+        best_enemy = None
+        best_dist = 2 * float(self.constants['worldsize'])
+        for enemy in self.enemies:
+            if enemy.status != 'alive':
+                continue
+            dist = math.sqrt((enemy.x - tank.x)**2 + (enemy.y - tank.y)**2)
+            if dist < best_dist:
+                best_dist = dist
+                best_enemy = enemy
+        if best_enemy is None:
+            command = Command(tank.index, 0, 0, False)
+            self.commands.append(command)
+        else:
+            self.move_to_position(tank, best_enemy.x, best_enemy.y)
+
+    def move_to_position(self, tank, target_x, target_y):
+        """Set command to move to given coordinates."""
+        target_angle = math.atan2(target_y - tank.y,
+                                  target_x - tank.x)
+        relative_angle = self.normalize_angle(target_angle - tank.angle)
+        command = Command(tank.index, 1, 2 * relative_angle, True)
+        self.commands.append(command)
+
+    def normalize_angle(self, angle):
+        """Make any angle be between +/- pi."""
+        angle -= 2 * math.pi * int (angle / (2 * math.pi))
+        if angle <= -math.pi:
+            angle += 2 * math.pi
+        elif angle > math.pi:
+            angle -= 2 * math.pi
+        return angle
+
+    def draw_grid(self):
+        # This assumes you are using a numpy array for your grid
+        width, height = self.grid.shape
+        glRasterPos2f(-1, -1)
+        glDrawPixels(width, height, GL_LUMINANCE, GL_FLOAT, self.grid)
+        glFlush()
+        glutSwapBuffers()
+
+    def update_grid(self, new_grid):
+        global grid
+        self.grid = new_grid
+
+    def init_window(self, width, height):
+        global window
+        global grid
+        glutInit(())
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+        glutInitWindowSize(width, height)
+        glutInitWindowPosition(0, 0)
+        window = glutCreateWindow("Grid filter")
+        glutDisplayFunc(self.draw_grid)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        #glutMainLoop()
+
+
+def main():
+    # Process CLI arguments.
+    try:
+        execname, host, port = sys.argv
+    except ValueError:
+        execname = sys.argv[0]
+        print >>sys.stderr, '%s: incorrect number of arguments' % execname
+        print >>sys.stderr, 'usage: %s hostname port' % sys.argv[0]
+        sys.exit(-1)
+
+    # Connect.
+    #bzrc = BZRC(host, int(port), debug=True)
+    bzrc = BZRC(host, int(port))
+
+    agent = Agent(bzrc)
+
+    prev_time = time.time()
+
+    # Run the agent
+    try:
+        while True:
+            time_diff = time.time() - prev_time
+            agent.tick(time_diff)
+    except KeyboardInterrupt:
+        print "Exiting due to keyboard interrupt."
+        bzrc.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: et sw=4 sts=4
